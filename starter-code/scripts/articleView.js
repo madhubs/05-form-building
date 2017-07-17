2b197f24cd77f1ac99536e8f6e551804cac7c487
Madeline Stevens <stevensm1987@gmail.com>
2017-07-16 18:33:47 -0700

    still able to toggle back and forth between the write and preview tabs, but there are still error messages in console from attempts at other TODOs. Michelle just left CF

--- starter-code/scripts/articleView.js
+++ starter-code/scripts/articleView.js
@@ -78,18 +78,23 @@ articleView.initNewArticlePage = function() {
   //   $('.tab-content').hide();
   //   $('#' + $(this).data('content')).show();
   // });
- // TODO: Make the tabs work. Right now, you're seeing all the tab content (items with a class of tab-content) on the page at once. The section with the id of "write" should show when the "write" tab is clicked; it is also the default and should be shown on page load. The section with the id of "articles" should show when the "preview" tab is clicked.
+ // DONE: Make the tabs work. Right now, you're seeing all the tab content (items with a class of tab-content) on the page at once. The section with the id of "write" should show when the "write" tab is clicked; it is also the default and should be shown on page load. The section with the id of "articles" should show when the "preview" tab is clicked.
   $('#articleForm').hide();
-
-
-  // TODO: Hide the article-export section on page load
+  $('.main-nav').on('click', 'li', function(){
+    $('#articleForm').show();
+  });
+  // DONE: Hide the article-export section on page load
 
   $('#article-json').on('focus', function(){
     this.select();
   });
 
-  // TODO: Add an event handler to update the preview and the article-export field if any inputs change.
 
+  articleView.showPreview = function(event){
+    event.preventDefault();
+    $('#articles').empty();
+  }
+  // TODO: Add an event handler to update the preview and the article-export field if any inputs change.
 
 };
 
@@ -102,28 +107,24 @@ articleView.initNewArticlePage = function() {
 
 
   // TODO: Instantiate an article based on what's in the form fields:
-  // var  = new formArticle ({
-  //   title:
-  //   body:
-  //   author:
-  //   URL:
-  //   category:
-  //   published:
-  // })
 
   // TODO: Use our interface to the Handblebars template to put the article preview into the DOM:
-
+// $('#articles').append($artTemp);
+// var $artTemp = document.getElementById('article-template')
 
   // TODO: The new articles we create will be shown as JSON in an element in our article-export section. From there, we can copy/paste the JSON into our source data file.
     // Set up this "export" functionality. When data is inputted into the form, that data should be converted to stringified JSON. Then, display that JSON in the element inside the article-export section. The article-export section was hidden on page load; make sure to show it as soon as data is entered in the form.
 
 // };
 
-// articleView.initNewArticlePage();
+
 articleView.initIndexPage = function() {
   articleView.populateFilters();
   articleView.handleCategoryFilter();
   articleView.handleAuthorFilter();
   articleView.handleMainNav();
   articleView.setTeasers();
+  articleView.initNewArticlePage();
+  articleView.showPreview();
+  articleView.init();
 };
